bdd737ba5cae81bd241beee542eb9663f5445ca9
ganeshpatro321 <ganeshpatro321@gmail.com>
2019-11-26 08:24:22 +0530

    Initial updates

---
+++ server/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  baseUrl: {
+    protocol: 'http',
+    hostname: 'api.openweathermap.org',
+    path: '/data/2.5/weather',
+  },
+
+  query: {
+    name: 'q',
+    id: 'id',
+    coordinates: {
+      latitude: 'lat',
+      longitude: 'lon',
+    },
+    zipcode: 'zip',
+  },
+
+  APIkey: 'ae41a5d841121f6fe0cfb9bb9e63e68d',
+};
